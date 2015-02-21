127ad4c71d6b0258ad08467dc08174b326382a41
Noah Mostacero <noahmostacero@gmail.com>
2015-02-20 16:47:09 -0800

    Video 26 done

--- js/resources.js
+++ js/resources.js
@@ -13,6 +13,7 @@ game.resources = [
         {name: "tower", type: "image", src: "data/img/tower_round.svg.png"},
 //        adds the creep
         {name: "creep1", type: "image", src: "data/img/brainmonster.png"},
+        {name: "title-screen", type: "image", src: "data/img/title.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
