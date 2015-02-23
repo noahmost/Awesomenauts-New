9cb70fddec9d18203f90109df231898c5c8f7f36
Noah Mostacero <noahmostacero@gmail.com>
2015-02-23 11:46:50 -0800

    Video 26 comments

--- js/game.js
+++ js/game.js
@@ -66,7 +66,7 @@ var game = {
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
 
-		// Start the game.
+		// Start the game. starts menu
 		me.state.change(me.state.MENU);
 	}
 };
--- js/screens/title.js
+++ js/screens/title.js
@@ -3,6 +3,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
+//            puts title screen up
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
 	},
 	
