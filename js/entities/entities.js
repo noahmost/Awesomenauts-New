d8b81dcc224e34ea849dd82faac995dd897c4718
Noah Mostacero <noahmostacero@gmail.com>
2015-02-25 11:15:33 -0800

    Video 30 finished created new functions

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,5 +1,21 @@
 game.PlayerEntity = me.Entity.extend ({
         init: function(x, y, settings){
+            this.setSuper();
+            this.setPlayerTimers();
+            this.setAttributes();
+            
+            this.type = "PlayerEntity";
+            this.setFlags();
+            this.lastHit = this.now;
+            this.lastAttack = new Date().getTime();
+//            this follows the position of the character
+            me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+            
+            this.addAnimation();
+            this.renderable.setCurrentAnimation("idle");
+            },
+        
+        setSuper: function(){
             this._super(me.Entity, "init", [x, y, {
             image: "player",
             width: 64,
@@ -11,28 +27,31 @@ game.PlayerEntity = me.Entity.extend ({
                 return(new me.Rect(0, 0, 64, 64)).toPolygon();
             }
              }]);
-         this.type = "PlayerEntity";
-//         sets the health for the player
-         this.health = game.data.playerHealth;
-//            this sets the speed that the character is going // sets movespeed to game.js
-            this.body.setVelocity(game.data.playerMoveSpeed, 20);
-            this.facing = "right";
-//            this updates the time and date of the hits
+        },
+        
+        setPlayerTimers: function(){
             this.now = new Date().getTime();
             this.lastHit = this.now;
-            this.dead = false;
-            this.attack = game.data.playerAttack;
             this.lastAttack = new Date().getTime();
-//            this follows the position of the character
-            me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
-            
+        },
+//        attributes are like the characters characteristics
+        setAttributes: function(){
+            this.health = game.data.playerHealth;
+//            this sets the speed that the character is going // sets movespeed to game.js
+            this.body.setVelocity(game.data.playerMoveSpeed, 20);
+            this.attack = game.data.playerAttack;
+        },
+        
+        setFlags: function(){
+        this.dead = false;
+        this.facing = "right";
+        },
+        
+        addAnimation: function(){
             this.renderable.addAnimation("idle", [78]);
             this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 123, 124, 125], 80);
             this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
-            
-            this.renderable.setCurrentAnimation("idle");
-            },
-        
+        },
         
                     
             update: function(delta){
