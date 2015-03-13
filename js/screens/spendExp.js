fad89eb53042271c34872674207cd7021852ad42
Noah Mostacero <noahmostacero@gmail.com>
2015-03-12 20:43:59 -0700

    Finished video 43

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -10,13 +10,18 @@ game.SpendExp = me.ScreenObject.extend({
                     
                     init: function(){
 //                        this adds the text to the title screen
-                        this._super(me.Renderable, 'init', [270, 240, 300, 50]);
+                        this._super(me.Renderable, 'init', [10, 10, 300, 50]);
 //                        this sets the font and color of the font on the title screen
                         this.font = new me.Font("Arial", 46, "white");
                     },
 //                    this is the text displayed on the screen
                     draw: function(renderer){
-                        this.font.draw(renderer.getContext(), "Spend", this.pos.x, this.pos.y);
+                        this.font.draw(renderer.getContext(), "Press F1- F4 to buy, f5 to skip", this.pos.x, this.pos.y);
+                        this.font.draw(renderer.getContext(), "Current EXP: " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
+                        this.font.draw(renderer.getContext(), "F1: Increase gold production " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 100);
+                        this.font.draw(renderer.getContext(), "F2:Add starting gold" + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 150);
+                        this.font.draw(renderer.getContext(), "f3: increase dmg " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 200);
+                        this.font.draw(renderer.getContext(), "f4: increase health " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 250);
                     }
                    
                 })));
