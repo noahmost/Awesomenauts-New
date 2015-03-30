59fa8b283cc9462e0515e8088471116db686c5e5
Noah Mostacero <noahmostacero@gmail.com>
2015-03-29 22:16:03 -0700

    Video 50 done

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -95,6 +95,8 @@ game.SpendGold = Object.extend({
             }
         }
         
+        this.checkBuyKeys();
+        
         return true;
     },
     startBuying: function(){
@@ -135,6 +137,7 @@ game.SpendGold = Object.extend({
 //                    this is the text displayed on the screen
                     draw: function(renderer){
 //                        these all edit text and text placements
+//                            displays text to buy menu
                         this.font.draw(renderer.getContext(), "Press F1- F6 to buy, B to skip. current gold" + game.data.gold, this.pos.x, this.pos.y);
                         this.font.draw(renderer.getContext(), " Dmg increase C" + game.data.skill1 + "Cost" + ((game.data.exp1+1) * 10), this.pos.x, this.pos.y + 40);
                         this.font.draw(renderer.getContext(), "Run fast "  + game.data.skill2 + "Cost" + ((game.data.exp2+1) * 10), this.pos.x, this.pos.y + 80);
@@ -164,5 +167,80 @@ game.SpendGold = Object.extend({
         me.input.unbindKey(me.input.KEY.F6, "F6", true);
 //        removes the buy text pause screen
         me.game.world.removeChild(game.data.buytext);
+    },
+    
+    checkBuyKeys: function(){
+        if(me.input.isKeyPressed("F1")){
+            if(this.checkCost(1)){
+               this.makePurchase(1); 
+            }
+        }else if(me.input.isKeyPressed("F2")){
+            if(this.checkCost(2)){
+               this.makePurchase(2); 
+            }
+        }else if(me.input.isKeyPressed("F3")){
+            if(this.checkCost(3)){
+               this.makePurchase(3); 
+            }
+        }else if(me.input.isKeyPressed("F4")){
+            if(this.checkCost(4)){
+               this.makePurchase(4); 
+            }
+        }else if(me.input.isKeyPressed("F5")){
+            if(this.checkCost(5)){
+               this.makePurchase(5); 
+            }
+        }else if(me.input.isKeyPressed("F6")){
+            if(this.checkCost(6)){
+               this.makePurchase(6); 
+            }
+        }
+    },
+    
+    checkCost: function(skill){
+        if(skill===1 && (game.data.gold >= ((game.data.skill1+1)*10))){
+            return true;
+        }else if(skill===2 && (game.data.gold >= ((game.data.skill2+1)*10))){
+            return true;
+        }else if(skill===3 && (game.data.gold >= ((game.data.skill3+1)*10))){
+            return true;
+        }else if(skill===4 && (game.data.gold >= ((game.data.ability1+1)*10))){
+            return true;
+        }else if(skill===5 && (game.data.gold >= ((game.data.ability2+1)*10))){
+            return true;
+        }else if(skill===6 && (game.data.gold >= ((game.data.ability3+1)*10))){
+            return true;
+        }else{
+            return false;
+        }
+    },
+    
+    makePurchase: function(skill){
+        if(skill === 1){
+            game.data.gold -= ((game.data.skill1 + 1)*10);
+            game.data.skill1 += 1;
+            game.data.player.attack += 1;
+        }
+    else if(skill ===2){
+        game.data.gold -= ((game.data.skill2 + 1)*10);
+            game.data.skill2 += 1;
+            game.data.player.attack += 1;
+    }else if(skill ===3){
+        game.data.gold -= ((game.data.skill3 + 1)*10);
+            game.data.skill3 += 1;
+            game.data.player.attack += 1;
+    }else if(skill === 4){
+        game.data.gold -= ((game.data.ability1 + 1)*10);
+            game.data.ability1 += 1;
+            game.data.player.attack += 1;
+    }else if(skill === 5){
+        game.data.gold -= ((game.data.ability2 + 1)*10);
+            game.data.ability2 += 1;
+            game.data.player.attack += 1;
+    }else if(skill === 6){
+     game.data.gold -= ((game.data.ability3 + 1)*10);
+            game.data.ability3 += 1;
+            game.data.player.attack += 1;   
     }
+}
 });
