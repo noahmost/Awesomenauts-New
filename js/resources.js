eaa199a0c960f45959aa66fadd5bfae566c742c7
Noah Mostacero <noahmostacero@gmail.com>
2015-04-06 22:12:57 -0700

    Video i4 done

--- js/resources.js
+++ js/resources.js
@@ -20,6 +20,7 @@ game.resources = [
         {name: "load-screen", type: "image", src: "data/img/loadpic.png"},
         {name: "new-screen", type: "image", src: "data/img/newpic.png"},
         {name: "spear", type: "image", src: "data/img/spear.png"},
+        {name: "minimap", type: "image", src: "data/img/minimap.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
