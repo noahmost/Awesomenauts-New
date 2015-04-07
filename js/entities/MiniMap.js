27ab1420029ef5f6592bd61887231be01f3f51d6
Noah Mostacero <noahmostacero@gmail.com>
2015-04-06 22:28:13 -0700

    I6 done

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,4 +1,16 @@
 game.MiniMap = me.Entity.extend({
-    
+    init: function(x, y, settings){
+        this._super(me.Entity, "init", [x, y,{
+               image: "minimap",
+               width: 281,
+               height: 158,
+               spritewidth: "281",
+               spriteheight: "158",
+               getShape: function(){
+                   return (new me.Rect(0, 0, 281, 158)).toPolygon();
+               }
+        }]);
+        this.floating = true;
+    }
 });
 
