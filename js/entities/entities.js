9aae28fc9f960d78cee44c887b97b6d5ea91a393
Noah Mostacero <noahmostacero@gmail.com>
2015-04-06 22:07:49 -0700

    I3 video done, spear gets thrown but no dmg

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -134,7 +134,7 @@ game.PlayerEntity = me.Entity.extend ({
             throwSpear: function(){
                 if(this.lastSpear >= game.data.spearTimer && game.data.ability3 >= 0){
               this.lastSpear = this.now;
-            var spear = me.pool.pull("spear", this.pos.x, this.pos.y, {});
+            var spear = me.pool.pull("spear", this.pos.x, this.pos.y, {}, this.facing);
             me.game.world.addChild(spear, 20);  
         }
             },
